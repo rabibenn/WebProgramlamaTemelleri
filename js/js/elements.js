e571553ff2e0833cdb94901050e5df739d710138
rabibenn <rrabiassultann@gmail.com>
2025-03-04 22:22:16 +0300

    Web Programlama Temelleri
    Okan Üniversitesi Bilgisayar Programcılığı 1. sınıf Web Programlama Temelleri Ders Notlarım

---
+++ js/js/elements.js
@@ -0,0 +1,46 @@
+// document.getElementById("div1");
+
+// let element = document.getElementById("div1");
+// console.log(element.style);
+// console.log(element.innerHTML);
+// console.log(element.innerText);
+// console.log(element.style.backgroundColor);
+
+
+// let element = document.getElementById("ul1");
+// console.log(element);
+// console.log(element.children[1]);
+
+
+// let element = document.getElementById("user");
+// console.log(element);
+// console.log(element.name);
+// console.log(element.value);
+
+
+// let element2 = document.getElementById("pass");
+// console.log(element2);
+// console.log(element2.value);
+
+
+let elements;
+elements = document.getElementsByTagName("div");
+console.log(elements);
+console.log(elements[2].style.backgroundColor);
+
+
+let liste;
+liste = document.getElementsByTagName("uş");
+console.log(liste);
+console.log(elements[3].style.backgroundColor);
+console.log(elements[2].innerText);
+
+// let girisler;
+// girisleririsler = document.getElementsByTagName("input");
+// console.log(girisler[1].name);
+// console.log(girisler[1].value);
+
+let elemanlar;
+elemanlar = document.getElementsByTagName("*");
+console.log(elemanlar);
+console.log(elemanlar[12].style.backgroundColor);
